3da6126ccabfa6b2fdf83edad8ee5f5251683fd4
Anthony Stevens <anthony@55zero.com>
2015-07-13 18:39:08 -0700

    Made necessary changes to account for new Reputation details in API

--- ExamplesLibrary/ExampleUtils.cs
+++ ExamplesLibrary/ExampleUtils.cs
@@ -34,7 +34,7 @@ namespace ExamplesLibrary
 
 			WriteLine(indent, "Best Name:                   {0}", person.BestName);
 			WriteLine(indent, "Names:                       {0}", String.Join(", ", person.Names));
-			WriteLine(indent, "Age Range:                   {0}", person.AgeRange == null ? "null" : person.AgeRange.ToString());
+			WriteLine(indent, "Age Range:                   {0}", person.AgeRange);
 			WriteLine(indent, "Gender:                      {0}", person.Gender);
 			WriteLine(indent, "Type:                        {0}", person.Type);
 
@@ -54,29 +54,29 @@ namespace ExamplesLibrary
 			PrintName(location, indent);
 			WriteLine(indent, "Address:                     {0}", location.Address);
 
-			WriteLine(indent, "Address Type:                {0}", location.AddressType.HasValue ? location.AddressType.Value.ToString() : "null");
-			WriteLine(indent, "Apartment Number:            {0}", String.IsNullOrWhiteSpace(location.AptNumber) ? "null" : location.AptNumber);
-			WriteLine(indent, "Box Number:                  {0}", String.IsNullOrWhiteSpace(location.BoxNumber) ? "null" : location.BoxNumber);
+			WriteLine(indent, "Address Type:                {0}", location.AddressType);
+			WriteLine(indent, "Apartment Number:            {0}", location.AptNumber);
+			WriteLine(indent, "Box Number:                  {0}", location.BoxNumber);
 			WriteLine(indent, "City:                        {0}", location.City);
 			WriteLine(indent, "CountryCode:                 {0}", location.CountryCode);
-			WriteLine(indent, "Deliverable:                 {0}", location.IsDeliverable.HasValue ? location.IsDeliverable.Value.ToString() : "null");
-			WriteLine(indent, "Delivery Point:              {0}", location.DeliveryPoint.HasValue ? location.DeliveryPoint.Value.ToString() : "null");
-			WriteLine(indent, "House:                       {0}", String.IsNullOrWhiteSpace(location.House) ? "null" : location.House);
+			WriteLine(indent, "Deliverable:                 {0}", location.IsDeliverable);
+			WriteLine(indent, "Delivery Point:              {0}", location.DeliveryPoint);
+			WriteLine(indent, "House:                       {0}", location.House);
 			WriteLine(indent, "Latitude/Longitude:          {0}", location.LatLong);
-			WriteLine(indent, "Not Receiving Mail Reason:   {0}", location.NotReceivingMailReason.HasValue ? location.NotReceivingMailReason.Value.ToString() : "null");
+			WriteLine(indent, "Not Receiving Mail Reason:   {0}", location.NotReceivingMailReason);
 			WriteLine(indent, "Postal Code:                 {0}", location.PostalCode);
-			WriteLine(indent, "Post Directional:            {0}", String.IsNullOrWhiteSpace(location.PostDir) ? "null" : location.PostDir);
-			WriteLine(indent, "Pre Directional:             {0}", String.IsNullOrWhiteSpace(location.PreDir) ? "null" : location.PreDir);
-			WriteLine(indent, "Receiving Mail:              {0}", location.IsReceivingMail.HasValue ? location.IsReceivingMail.Value.ToString() : "null");
+			WriteLine(indent, "Post Directional:            {0}", location.PostDir);
+			WriteLine(indent, "Pre Directional:             {0}", location.PreDir);
+			WriteLine(indent, "Receiving Mail:              {0}", location.IsReceivingMail);
 			WriteLine(indent, "Standard Address Line1:      {0}", location.StandardAddressLine1);
 			WriteLine(indent, "Standard Address Line2:      {0}", location.StandardAddressLine2);
 			WriteLine(indent, "Standard Address Location:   {0}", location.StandardAddressLocation);
 			WriteLine(indent, "State Code:                  {0}", location.StateCode);
-			WriteLine(indent, "Street Name:                 {0}", String.IsNullOrWhiteSpace(location.StreetName) ? "null" : location.StreetName);
-			WriteLine(indent, "Street Type:                 {0}", String.IsNullOrWhiteSpace(location.StreetType) ? "null" : location.StreetType);
-			WriteLine(indent, "Usage:                       {0}", location.Usage.HasValue ? location.Usage.Value.ToString() : "null");
-			WriteLine(indent, "ValidFor:                    {0}", location.ValidFor == null ? "null" : location.ValidFor.ToString());
-			WriteLine(indent, "Zip4:                        {0}", String.IsNullOrWhiteSpace(location.Zip4) ? "null" : location.Zip4);
+			WriteLine(indent, "Street Name:                 {0}", location.StreetName);
+			WriteLine(indent, "Street Type:                 {0}", location.StreetType);
+			WriteLine(indent, "Usage:                       {0}", location.Usage);
+			WriteLine(indent, "ValidFor:                    {0}", location.ValidFor);
+			WriteLine(indent, "Zip4:                        {0}", location.Zip4);
 
 			DumpBaseEntity(location, depth, indent);
 		}
@@ -84,13 +84,13 @@ namespace ExamplesLibrary
 		public static void DumpPhone(IPhone phone, int depth, int indent = 0)
 		{
 			PrintName(phone, indent);
-			WriteLine(indent, "Carrier:                     {0}", String.IsNullOrWhiteSpace(phone.Carrier) ? "null" : phone.Carrier);
+			WriteLine(indent, "Carrier:                     {0}", phone.Carrier);
 			WriteLine(indent, "Country Calling Code:        {0}", phone.CountryCallingCode);
-			WriteLine(indent, "Do Not Call:                 {0}", phone.DoNotCall.HasValue ? phone.DoNotCall.Value.ToString() : "null");
-			WriteLine(indent, "Extension:                   {0}", phone.Extension ?? "null");
+			WriteLine(indent, "Do Not Call:                 {0}", phone.DoNotCall);
+			WriteLine(indent, "Extension:                   {0}", phone.Extension);
 			WriteLine(indent, "LineType:                    {0}", phone.LineType);
 			WriteLine(indent, "PhoneNumber:                 {0}", phone.PhoneNumber);
-			WriteLine(indent, "Is Prepaid:                  {0}", phone.IsPrepaid.HasValue ? phone.IsPrepaid.Value.ToString() : "null");
+			WriteLine(indent, "Is Prepaid:                  {0}", phone.IsPrepaid);
 			WriteLine(indent, "Reputation:                  {0}", GetSpamScore(phone));
 			WriteLine(indent, "Is Valid:                    {0}", phone.IsValid);
 
@@ -111,7 +111,7 @@ namespace ExamplesLibrary
 		{
 			if (depth > 1 && location != null)
 			{
-				WriteLine(indent, "Best Location:               {0}", location.Name);
+				WriteLine(indent, "Best Location: {0,15}", location.Name);
 			}
 		}
 
@@ -197,14 +197,14 @@ namespace ExamplesLibrary
 
 		private static void DumpLocationAssociation(LocationAssociation association, int depth, int indent)
 		{
-			WriteLine(indent, "Contact Type:                {0}", association.ContactType.HasValue ? association.ContactType.Value.ToString() : "null");
+			WriteLine(indent, "Contact Type:                {0}", association.ContactType);
 			WriteLine(indent, "Location:");
 			DumpLocation(association.Location, depth, indent + DefaultIndent);
 		}
 
 		private static void DumpPhoneAssociation(PhoneAssociation association, int depth, int indent)
 		{
-			WriteLine(indent, "Contact Type:                {0}", association.ContactType.HasValue ? association.ContactType.Value.ToString() : "null");
+			WriteLine(indent, "Contact Type:                {0}", association.ContactType);
 			WriteLine(indent, "Phone:");
 			DumpPhone(association.Phone, depth, indent + DefaultIndent);
 
@@ -213,7 +213,7 @@ namespace ExamplesLibrary
 
 		private static void PrintName(IEntity entity, int indent)
 		{
-			WriteLine(indent, "Name:                        {0}", entity.Name);
+			WriteLine(indent, "Name:{0,24}", entity.Name);
 		}
 
 		private static void WriteLine(int indent, string format, Object value = null)
